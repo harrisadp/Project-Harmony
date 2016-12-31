4ce12ea9510f38bf513dc9461f8cedcd046ddffa
Alex Harris <harris.adp@gmail.com>
2016-12-31 15:51:48 -0500

    SourceTree works. Hallelujah.

--- Assets/Scripts/Disease.cs
+++ Assets/Scripts/Disease.cs
@@ -24,7 +24,6 @@ private int respiratoryRate;
 		systolicBP = Random.Range(115, 130);
 		diastolicBP = Random.Range (75, 90);
 		respiratoryRate = Random.Range (10, 14);
-		Debug.Log ("Started");
 	}
 	
 	// Update is called once per frame
