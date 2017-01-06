8cf6235fed35cfd72977c33e889b4788ce69d75d
Alex Harris <harris.adp@gmail.com>
2017-01-05 23:37:16 -0500

    Added dictionary structure for default lab values

---
+++ Assets/Scripts/DefaultLabValues.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultLabValues : MonoBehaviour {
+
+	public Dictionary<string, float> labValues = new Dictionary<string, float>();
+
+	void Awake () {
+		labValues ["WBC"] = Random.Range (3.5f, 10.5f);
+		labValues ["HGB"] = Random.Range (13.5f, 15.5f);
+		labValues ["PLT"] = Random.Range (150f, 450f);
+		labValues ["Na"] = Random.Range (133f, 143f);
+		labValues ["K"] = Random.Range (3.5f, 5.1f);
+		labValues ["Cl"] = Random.Range (98f, 107f);
+		labValues ["HCO3"] = Random.Range (22f, 30f);
+		labValues ["BUN"] = Random.Range (7f, 20f);
+		labValues ["Cr"] = Random.Range (0.6f, 1.2f);
+		labValues ["Glu"] = Random.Range (70f, 100f);
+		labValues ["AST"] = Random.Range (8f, 48f);
+		labValues ["ALT"] = Random.Range (7f, 55f);
+		Debug.Log ("Default lab value set to: " + labValues["WBC"]);
+		Debug.Log ("Default lab value set to: " + labValues["Cl"]);
+		Debug.Log ("Default lab value set to: " + labValues["ALT"]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Disease.cs
+++ Assets/Scripts/Disease.cs
@@ -1,45 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Disease : MonoBehaviour {
 
-public Text tText, hrText, sbpText, dbpText, rrText, spO2Text, labResults1, labResults2, labResults3;
-
-private float temperature, spO2;
-private int heartRate, systolicBP, diastolicBP, respiratoryRate;
-
-private float wbc, hgb, plt;
+	private DefaultLabValues labs;
 
 	// Use this for initialization
 	void Start () {
-		temperature = Random.Range(36.5f, 37.9f);
-		heartRate = Random.Range(60,80);
-		systolicBP = Random.Range(115, 130);
-		diastolicBP = Random.Range (75, 90);
-		respiratoryRate = Random.Range (10, 14);
-		spO2 = Random.Range (0.9f, 1.0f);
-		wbc = Random.Range (3.5f, 10.5f);
-		hgb = Random.Range (12.0f, 17.5f);
-		plt = Random.Range (150f, 450f);
+		labs = GetComponent<DefaultLabValues> ();
+		labs.labValues ["WBC"] = UnityEngine.Random.Range (15.0f, 25.0f);
+		Debug.Log ("New WBC value is: " + labs.labValues ["WBC"]);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		tText.text = temperature.ToString ("F1");
-		hrText.text = heartRate.ToString ();
-		sbpText.text = systolicBP.ToString ();
-		dbpText.text = diastolicBP.ToString ();
-		rrText.text = respiratoryRate.ToString ();
-		spO2Text.text = spO2.ToString("P0");
-	}
 
-	public void CBC () {
-		Debug.Log ("Button clicked");
-		labResults1.text = wbc.ToString("F1");
-		labResults2.text = hgb.ToString("F1");
-		labResults3.text = plt.ToString("F0");
 	}
-
+		
 }
