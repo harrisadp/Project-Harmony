482bfa7cbab82c03aa028676a585e44deaa0a544
Alex Harris <harris.adp@gmail.com>
2017-01-09 14:28:08 -0500

    Wired buttons to BattleMenuManager script, filled out dialogue in QuickPlay text

--- Assets/Scripts/BattleMenuManager.cs
+++ Assets/Scripts/BattleMenuManager.cs
@@ -11,32 +11,56 @@ public class BattleMenuManager : MonoBehaviour {
 	public GameObject labs;
 	public GameObject imaging;
 
+	// History
+	public GameObject hpi;
+	public GameObject pmh;
+	public GameObject psh;
+	public GameObject sh;
+	public GameObject fh;
+	public GameObject backHistory;
+
+	// Physical
+	public GameObject heent;
+	public GameObject card;
+	public GameObject pulm;
+	public GameObject abd;
+	public GameObject neuro;
+	public GameObject msk;
+	public GameObject skin;
+	public GameObject backPhysical;
+
 	// Labs
 	public GameObject blood;
 	public GameObject urine;
 	public GameObject csf;
+	public GameObject backLabs;
 
 	// Blood
 	public GameObject cbc;
 	public GameObject bmp;
+	public GameObject lft;
+	public GameObject coags;
+	public GameObject backBlood;
+
+	// Imaging
+	public GameObject xray;
+	public GameObject ct;
+	public GameObject mri;
+	public GameObject us;
+	public GameObject pet;
+	public GameObject backImaging;
 
 	// Use this for initialization
 	void Start () {
-		foreach (Transform child in transform) {
-			child.gameObject.SetActive (false);
-		}
+		foreach (Transform child in transform) {child.gameObject.SetActive (false);}
 	}
 
 	public void Reset () {
-		foreach (Transform child in transform) {
-			child.gameObject.SetActive (false);
-		}
+		foreach (Transform child in transform) {child.gameObject.SetActive (false);}
 	}
 
 	public void Initialize () {
-		foreach (Transform child in transform) {
-			child.gameObject.SetActive (false);
-		}
+		foreach (Transform child in transform) {child.gameObject.SetActive (false);}
 		
 		history.SetActive (true);
 		physical.SetActive (true);
@@ -44,23 +68,53 @@ public class BattleMenuManager : MonoBehaviour {
 		imaging.SetActive (true);
 	}
 
-	public void Labs () {
-		foreach (Transform child in transform) {
-			child.gameObject.SetActive (false);
-		}
+	public void History () {
+		foreach (Transform child in transform) {child.gameObject.SetActive (false);}
+		hpi.SetActive (true);
+		pmh.SetActive (true);
+		psh.SetActive (true);
+		sh.SetActive (true);
+		fh.SetActive (true);
+		backHistory.SetActive (true);
+	}
 
+	public void Physical () {
+		foreach (Transform child in transform) {child.gameObject.SetActive (false);}
+		heent.SetActive (true);
+		card.SetActive (true);
+		pulm.SetActive (true);
+		abd.SetActive (true);
+		neuro.SetActive (true);
+		msk.SetActive (true);
+		skin.SetActive (true);
+		backPhysical.SetActive (true);
+	}
+
+	public void Labs () {
+		foreach (Transform child in transform) {child.gameObject.SetActive (false);}
 		blood.SetActive (true);
 		urine.SetActive (true);
 		csf.SetActive (true);
+		backLabs.SetActive (true);
 	}
 
 	public void Blood () {
-		foreach (Transform child in transform) {
-			child.gameObject.SetActive (false);
-		}
-
+		foreach (Transform child in transform) {child.gameObject.SetActive (false);}
 		cbc.SetActive (true);
 		bmp.SetActive (true);
+		lft.SetActive (true);
+		coags.SetActive (true);
+		backBlood.SetActive (true);
+	}
+
+	public void Imaging () {
+		foreach (Transform child in transform) {child.gameObject.SetActive (false);}
+		xray.SetActive (true);
+		ct.SetActive (true);
+		mri.SetActive (true);
+		us.SetActive (true);
+		pet.SetActive (true);
+		backImaging.SetActive (true);
 	}
 
 }
--- Assets/Scripts/DefaultLabValues.cs
+++ Assets/Scripts/DefaultLabValues.cs
@@ -19,6 +19,14 @@ public class DefaultLabValues : MonoBehaviour {
 		labValues ["Glu"] = Mathf.Round(Random.Range (70f, 100f));
 		labValues ["AST"] = Mathf.Round(Random.Range (8f, 48f));
 		labValues ["ALT"] = Mathf.Round(Random.Range (7f, 55f));
+		labValues ["AlkPhos"] = Mathf.Round(Random.Range (88f, 126f));
+		labValues ["Ca"] = Mathf.Round(10*(Random.Range (8.4f, 10.2f)))/10;
+		labValues ["TotalProt"] = Mathf.Round(10*(Random.Range (6.0f, 10.0f)))/10;
+		labValues ["Albumin"] = Mathf.Round(10*(Random.Range (3.5f, 5.5f)))/10;
+		labValues ["TotalBili"] = Mathf.Round(10*(Random.Range (0.0f, 1.4f)))/10;
+		labValues ["PT"] = Mathf.Round(10*(Random.Range (11.1f, 13.1f)))/10;
+		labValues ["PTT"] = Mathf.Round(10*(Random.Range (22.1f, 35.1f)))/10;
+		labValues ["INR"] = Mathf.Round(10*(Random.Range (0.8f, 1.2f)))/10;
 	}
 	
 	// Update is called once per frame
