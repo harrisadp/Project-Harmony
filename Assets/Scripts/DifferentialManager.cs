c04a373b99ee9bb6eaf74b60bbe467602e2f27ba
Alex Harris <harris.adp@gmail.com>
2017-01-10 15:18:09 -0500

    Implemented Simple Drag and Drop bundle from Asset Store. Got the basic gameplay flow (differential —> gather information —> reassess differential) working, but the differential items are still using placeholders

---
+++ Assets/Scripts/DifferentialManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifferentialManager : MonoBehaviour {
+
+	private BattleMenuManager battleMenuManager;
+
+	// Use this for initialization
+	void Start () {
+		battleMenuManager = FindObjectOfType<BattleMenuManager> ();
+		gameObject.SetActive (false);
+	}
+
+	public void Enable () {
+		gameObject.SetActive (true);
+	}
+
+	public void NextTurn () {
+		gameObject.SetActive (false);
+		battleMenuManager.Initialize ();
+	}
+
+}
