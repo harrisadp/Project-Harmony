b20ba597d92f74d8385758bf00afb6f1552c787d
Alex Harris <harris.adp@gmail.com>
2017-01-18 21:06:09 -0500

    Did some renaming and cleaning up after the merge. Still doesn’t compile.

--- Assets/Scripts/DiseaseChooser.cs
+++ Assets/Scripts/DiseaseChooser.cs
@@ -5,19 +5,16 @@ using AssemblyCSharp;
 
 public class DiseaseChooser : MonoBehaviour {
 
-	//Name collision. I don't know how c# would handle enum and class of same name
-	//Try DiseaseId for enum name
-	public enum DiseaseId {disease1, disease2, disease3};
-	private DiseaseId diseaseChosen;
-	private Disease disease_data;
+	public enum DiseaseID {disease1, disease2, disease3};
+	private int diseaseChosen;
+	private Person disease_data;
 
 	// Use this for initialization
 	void Start () {
-		//I would pass the identifier you received into some larger struct that handles the actual data.
-		diseaseChosen = (DiseaseId)Random.Range (0, 3);
-		disease_data = BigBadDiseaseStruct.GetDiseaseFromList(diseaseChosen); //Using the class name of BigBadDiseaseStruct, but should actually be the name of the variable the constructor was assigned to
+		diseaseChosen = (int)(DiseaseID)Random.Range (0, 3);
+		disease_data = DiseaseStruct.GetDiseaseFromList(diseaseChosen);
 
-		//since all data should exist with the disease_data you just pulled, you only need to call the methods for that data. Don't need to worry about passing any other information.
+		// since all data should exist with the disease_data you just pulled, you only need to call the methods for that data. Don't need to worry about passing any other information.
 		int age = disease_data.AgeGenerator ();
 		bool male = disease_data.SexGenerator ();
 		int race = disease_data.RaceGenerator ();
---
+++ Assets/Scripts/DiseaseStruct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class DiseaseStruct
+	{
+		private Person disease_list;
+
+		//This constructor should be called once in some start up code that runs at program start
+		public DiseaseStruct (int size)
+		{
+			disease_list = new Person[size];
+
+			//The benefit of storing the data like this is that it would be really easy to generate this from an excel sheet
+			disease_list[0] = new Person("Asthma", 20, 60, 0.5f, 0.25f, 0.25f, 0.25f, 0.25f);
+			disease_list[1] = new Person(/*Next Disease*/);
+		}
+
+		public Person GetDiseaseFromList(int index)
+		{
+			return disease_list [index];
+		}
+
+	}
+}
+
