bc452e3e54e4a4b7c208e9cb6e9000f7a1dca423
Alex Harris <harris.adp@gmail.com>
2017-01-18 21:47:11 -0500

    Got things to compile, but I had to take away the AssemblyCSharp references. Not sure if that’s okay or not.

--- Assets/Scripts/DiseaseStruct.cs
+++ Assets/Scripts/DiseaseStruct.cs
@@ -1,23 +1,19 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace AssemblyCSharp {
-	
-	public class DiseaseStruct {
+public class DiseaseStruct : MonoBehaviour {
 		
-		private Person[] disease_list;
+	private Person[] disease_list;
 
-		public DiseaseStruct (int size) {
-			disease_list = new Person[size];
-			disease_list[0] = new Person("Asthma", 5, 10, 0.5f, 0.25f, 0.25f, 0.25f, 0.25f);
-			disease_list[1] = new Person("Bronchitis", 20, 50, 0.5f, 0.25f, 0.25f, 0.25f, 0.25f);
-			disease_list[2] = new Person("COPD", 60, 90, 0.9f, 0.25f, 0.25f, 0.25f, 0.25f);
-		}
-
-		public Person GetDiseaseFromList(int index) {
-			return disease_list [index];
-		}
+	public DiseaseStruct (int size) {
+		disease_list = new Person[size];
+		disease_list[0] = new Person("Asthma", 5, 10, 0.5f, 0.25f, 0.25f, 0.25f, 0.25f);
+		disease_list[1] = new Person("Bronchitis", 20, 50, 0.5f, 0.25f, 0.25f, 0.25f, 0.25f);
+		disease_list[2] = new Person("COPD", 60, 90, 0.9f, 0.25f, 0.25f, 0.25f, 0.25f);
 	}
 
+	public Person GetDiseaseFromList(int index) {
+		return disease_list [index];
+	}
 }
-
--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -5,37 +5,23 @@ using UnityEngine.UI;
 
 public class Person {
 
-	private enum Race {asian, black, hispanic, white};
-	private enum Personality {personality1, personality2, personality3};
+	public enum Race {asian, black, hispanic, white};
+	public enum Personality {personality1, personality2, personality3};
 
 	private string disease_name;
-	private int age_min;
-	private int age_max;
-	private float male_probability;
-	private float asian_probability;
-	private float black_probability;
-	private float hispanic_probability;
-	private float white_probability;
+	private int age;
+	private bool male;
+	private Race race;
 	private Personality personality;
 
 	public Person (string diseaseName, int ageMin, int ageMax, float maleProbability, float asianProbability, float blackProbability, float hispanicProbability, float whiteProbability) {
 		Debug.Log ("Disease constructor run");
 		Debug.Log ("Disease name is " + diseaseName);
 		this.disease_name = diseaseName;
-		this.age_min = ageMin;
-		this.age_max = ageMax;
-		this.male_probability = maleProbability;
-		this.asian_probability = asianProbability;
-		this.black_probability = blackProbability;
-		this.hispanic_probability = hispanicProbability;
-		this.white_probability = whiteProbability;
-		Debug.Log (age_min);
-		Debug.Log (age_max);
-		Debug.Log (male_probability);
-		Debug.Log (asianProbability);
-		Debug.Log (black_probability);
-		Debug.Log (hispanic_probability);
-		Debug.Log (white_probability);
+		Debug.Log (age);
+		Debug.Log (male);
+		Debug.Log (race);
+		Debug.Log (personality);
 	}
 
 }
