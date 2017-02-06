f8af76654c2790c20a1e83f3da1e4df168eb029c
Alex Harris <harris.adp@gmail.com>
2017-02-06 16:17:25 -0500

    Added Background Chooser button to UI.

---
+++ Assets/Scripts/BackgroundChooser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChooser : MonoBehaviour {
+
+	public Sprite[] backgrounds;
+
+	private int currentBackground;
+	private GameObject background;
+
+	// Use this for initialization
+	void Start () {
+		background = GameObject.Find ("Background");
+		background.GetComponent<SpriteRenderer> ().sprite = backgrounds [0];
+		currentBackground = 0;
+	}
+
+	public void NewBackground () {
+		if (currentBackground < backgrounds.Length - 1) {
+			currentBackground += 1;
+		} else {
+			currentBackground = 0;
+		}
+		background.GetComponent<SpriteRenderer> ().sprite = backgrounds [currentBackground];
+	}
+
+}
